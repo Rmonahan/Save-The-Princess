7fff1e8ee998a031e299afd1f67a8c5fe9f14337
Rmonahan <monahan.62@osu.edu>
2020-01-10 21:10:49 -0500

    Added title and controls and links to page

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ const GameView = require("./game_view");
 document.addEventListener("DOMContentLoaded", function () {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
-
-  // backgroundCanvas = document.getElementById("myCanvas2");
-  // backgroundCtx = backgroundCanvas.getContext("2d");
  
   let level = new Level(0, ctx, canvas);
   new GameView(level, ctx, canvas);
